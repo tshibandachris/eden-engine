9da58e1ec9d97a240a906cf6cca448f4ca3fff52
tshibandachris <tshibandachris@gmail.com>
2025-10-09 00:03:19 +0100

    Delete index.jsx

--- index.jsx
+++
@@ -1,4 +0,0 @@
-
-import {useEffect} from 'react';
-import {useRouter} from 'next/router';
-export default function Home(){ const r=useRouter(); useEffect(()=>r.replace('/matches'),[]); return <div className="min-h-screen flex items-center justify-center">Redirection…</div> }
